1963a660a0d4312c1b4735f80480e5642d2e032d
Pedro Barreto <phlbarreto90@gmail.com>
2026-04-12 01:26:34 -0300

    refactor: renomeia rota para criar usuário

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,21 +1,22 @@
 import { Router } from "express";
 import { index } from "~/controller";
-import { login, register, validate } from "~/controller/auth";
+import { authentication, register, getUser } from "~/controller/user";
 import {
   deleteTask,
   getTasks,
   insertTask,
   updateTask,
 } from "~/controller/task";
+
 import { authenticate } from "~/middleware/authenticate";
 
 const router = Router();
 
 router.get("/", index);
 
-router.post("/register", register);
-router.post("/login", login);
-router.get("/validate", validate);
+router.post("/login", authentication);
+router.post("/user", register);
+router.get("/user", getUser);
 
 router.get("/tasks", authenticate, getTasks);
 router.post("/tasks", authenticate, insertTask);
