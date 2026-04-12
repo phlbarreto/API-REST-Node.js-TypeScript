3d7cf344ee6e307d8ea2647cdb0eb9e90d1c37f2
Pedro Barreto <phlbarreto90@gmail.com>
2026-04-12 11:15:11 -0300

    feat: acrescenta rota para buscar uma tarefa

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,7 +3,8 @@ import { index } from "~/controller";
 import { authentication, register, getUser } from "~/controller/user";
 import {
   deleteTask,
-  getTasks,
+  getAllTasks,
+  getOneTask,
   insertTask,
   updateTask,
 } from "~/controller/task";
@@ -18,7 +19,8 @@ router.post("/login", authentication);
 router.post("/user", register);
 router.get("/user", getUser);
 
-router.get("/tasks", authenticate, getTasks);
+router.get("/tasks", authenticate, getAllTasks);
+router.get("/tasks/:id", authenticate, getOneTask)
 router.post("/tasks", authenticate, insertTask);
 router.patch("/tasks/:id", authenticate, updateTask);
 router.delete("/tasks/:id", authenticate, deleteTask);
