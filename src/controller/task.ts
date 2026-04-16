367d2e89cd4df1ca549104e7553b1860f7b51664
Pedro Barreto <phlbarreto90@gmail.com>
2026-04-15 22:34:30 -0300

    refactor: modifica o CRUD de tasks de funções para classe

--- src/controller/task.ts
+++ src/controller/task.ts
@@ -3,8 +3,8 @@ import { Task } from "@/generated/client";
 import { idTaskSchema, taskSchema, updateTaskSchema } from "~/zod/taskSchema";
 import { AuthenticatedRequest } from "~/middleware/authenticate";
 import { canRequest } from "~/models/authorization";
-import { CustomResponse } from "~/models/response";
-import { create, deleteOne, findAll, findOne, update } from "~/models/task";
+import { CustomResponse } from "~/models/customResponse";
+import { taskModel } from "~/models/task";
 
 export const getAllTasks = async (req: AuthenticatedRequest, res: Response) => {
   const response = new CustomResponse(res);
@@ -12,12 +12,15 @@ export const getAllTasks = async (req: AuthenticatedRequest, res: Response) => {
   if (!user) return;
 
   try {
-    const tasks = await findAll(user.id);
+    const tasks = await taskModel.findAll(user.id);
     if (!tasks) {
-      response.noContent("Nenhuma tarefa encontrada.");
+      response.noContent();
       return;
     }
-    response.success(`Total de ${tasks.length} tarefa(s) encontrada(s)`, tasks);
+    response.success({
+      data: tasks,
+      message: `Total de ${tasks.length} tarefa(s) encontrada(s)`,
+    });
   } catch (error) {
     console.error("Erro ao buscar tasks: ", error);
     response.internalServerError();
@@ -36,7 +39,7 @@ export const insertTask = async (req: AuthenticatedRequest, res: Response) => {
   if (!user) return;
 
   try {
-    const task = await create(result.data, user.id);
+    const task = await taskModel.create(result.data, user.id);
     response.created(`Task ${task.id} adicionada com sucesso!`, task);
   } catch (error) {
     console.error("Erro ao inserir task: ", error);
@@ -61,7 +64,7 @@ export const updateTask = async (req: AuthenticatedRequest, res: Response) => {
 
     if (!userCanRequest(req, response, task)) return;
 
-    const updatedTask = await update(taskId, result.data);
+    const updatedTask = await taskModel.update(taskId, result.data);
     response.created(`Task ${taskId} atualizada.`, updatedTask);
   } catch (error) {
     console.error("Erro ao atualizar tasks: ", error);
@@ -80,8 +83,8 @@ export const deleteTask = async (req: AuthenticatedRequest, res: Response) => {
 
     if (!userCanRequest(req, response, task)) return;
 
-    await deleteOne(taskId);
-    response.success(`Task ${taskId} deletada com sucesso!`);
+    await taskModel.deleteOne(taskId);
+    response.success({ message: `Task ${taskId} deletada com sucesso!` });
   } catch (error) {
     console.error("Erro ao deletar task: ", error);
     response.internalServerError();
@@ -99,7 +102,7 @@ export const getOneTask = async (req: AuthenticatedRequest, res: Response) => {
 
     if (!userCanRequest(req, response, task)) return;
 
-    response.success("", task);
+    response.success({ task });
   } catch (error) {
     console.error("Erro ao buscar task: ", error);
     response.internalServerError();
@@ -127,7 +130,7 @@ function userCanRequest(
 ) {
   const user = req.context?.user;
   if (!user) {
-    response.unauthorized("Não autorizado", "Faça o login novamente");
+    response.unauthorized();
     return false;
   }
 
@@ -143,9 +146,9 @@ async function findTaskOrResponseNoContent(
   taskId: string,
   response: CustomResponse,
 ) {
-  const task = await findOne(taskId);
+  const task = await taskModel.findOne(taskId);
   if (!task) {
-    response.noContent("Nenhuma tarefa encontrada para o id enviado.");
+    response.noContent();
     return false;
   }
   return task;
@@ -157,7 +160,7 @@ function getUserFromContext(
 ) {
   const user = req.context?.user;
   if (!user) {
-    response.unauthorized("Não autorizado!", "Faça o login.");
+    response.unauthorized("Faça o login.");
     return false;
   }
   return user;
--- src/models/task.ts
+++ src/models/task.ts
@@ -1,61 +1,70 @@
+import { TaskDelegate } from "@/generated/models";
 import { prisma } from "~/infra/database";
-import { Task, UpdateTask } from "~/zod/taskSchema";
+import { Task as TaskEnt, UpdateTask } from "~/zod/taskSchema";
 
-export const findAll = async (userId: string) => {
-  const tasks = await prisma.task.findMany({
-    where: {
-      user_id: userId,
-    },
-    take: 15,
-    orderBy: {
-      updated_at: "desc",
-    },
-  });
+class TaskModel {
+  private model: TaskDelegate;
+  constructor() {
+    this.model = prisma.task;
+  }
 
-  if (tasks.length === 0) return null;
+  async findAll(userId: string) {
+    const tasks = await this.model.findMany({
+      where: {
+        user_id: userId,
+      },
+      take: 15,
+      orderBy: {
+        updated_at: "desc",
+      },
+    });
 
-  return tasks;
-};
+    if (tasks.length === 0) return null;
 
-export const create = async (providedData: Task, user_id: string) => {
-  const data = {
-    ...providedData,
-    user_id,
-  };
+    return tasks;
+  }
 
-  const task = await prisma.task.create({
-    data,
-  });
+  async create(providedData: TaskEnt, user_id: string) {
+    const data = {
+      ...providedData,
+      user_id,
+    };
 
-  return task;
-};
+    const task = await this.model.create({
+      data,
+    });
 
-export const findOne = async (id: string) => {
-  const task = await prisma.task.findFirst({
-    where: { id },
-  });
+    return task;
+  }
 
-  if (!task) return null;
+  async findOne(id: string) {
+    const task = await this.model.findFirst({
+      where: { id },
+    });
 
-  return task;
-};
+    if (!task) return null;
 
-export const update = async (id: string, providedData: UpdateTask) => {
-  const now = new Date();
-  const data = {
-    ...providedData,
-    updated_at: now,
-  };
+    return task;
+  }
 
-  const updatedTask = await prisma.task.update({
-    where: { id },
-    data,
-  });
-  return updatedTask;
-};
+  async update(id: string, providedData: UpdateTask) {
+    const now = new Date();
+    const data = {
+      ...providedData,
+      updated_at: now,
+    };
 
-export const deleteOne = async (id: string) => {
-  await prisma.task.delete({
-    where: { id },
-  });
-};
+    const updatedTask = await this.model.update({
+      where: { id },
+      data,
+    });
+    return updatedTask;
+  }
+  async deleteOne(id: string) {
+    await this.model.delete({
+      where: { id },
+    });
+  }
+}
+
+export const taskModel = new TaskModel();
