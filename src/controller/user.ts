f52b7330c7d935ace4ad63ebe20f136293a7ae41
Pedro Barreto <phlbarreto90@gmail.com>
2026-04-15 22:41:03 -0300

    refactor: refatorações gerais

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import { loginSchema } from "~/zod/userSchema";
 import { registerSchema } from "~/zod/userSchema";
-import { validateSession } from "~/models/session";
+import { AuthenticatedUser, validateSession } from "~/models/session";
 import { registerUser, login } from "~/models/user";
-import { CustomResponse } from "~/models/response";
+import { CustomResponse } from "~/models/customResponse";
 import { CookieResponse } from "~/models/cookieResponse";
 
 export const register = async (req: Request, res: Response) => {
@@ -17,7 +17,9 @@ export const register = async (req: Request, res: Response) => {
 
   try {
     const registeredUser = await registerUser(result.data);
-    response.success(`Usuário '${registeredUser.name}' criado com sucesso!`);
+    response.success({
+      message: `Usuário '${registeredUser.name}' criado com sucesso!`,
+    });
   } catch (error: any) {
     console.error("Erro ao registrar usuário: ", error);
     response.internalServerError();
@@ -36,44 +38,44 @@ export const authentication = async (req: Request, res: Response) => {
     const sessionObject = await login(result.data);
     if (!sessionObject.success) {
       response.unauthorized(
-        "Email ou senha inválido",
         "Verifique os dados enviados e tente novamente",
+        "Email ou senha inválido",
       );
       return;
     }
 
     new CookieResponse(res).setCookie("session", sessionObject.session);
-    response.success("Login efetuado com sucesso");
+    response.success({ message: "Usuário autenticado!" });
   } catch (error) {
     console.error("Erro insperado ao efetuar login: ", error);
     response.internalServerError();
   }
 };
 
-export const getUser = async (req: Request, res: Response) => {
+export const getUser = async (
+  req: Request,
+  res: Response,
+): Promise<AuthenticatedUser | undefined> => {
   const sessionId = req.cookies.session;
   const response = new CookieResponse(res);
   if (!sessionId) {
-    response.unauthorized(
-      "Não autorizado.",
-      "Verifique se está logado e tente novamente.",
-    );
+    response.unauthorized("Verifique se está logado e tente novamente.");
     return;
   }
 
   try {
-    const userOrError = await validateSession(sessionId);
+    const user = await validateSession(sessionId);
 
-    if (!userOrError) {
+    if (!user) {
       response.clearCookie("session");
       response.unauthorized(
-        "Sessão inválida ou expirada.",
         "Por favor, realize o login novamente.",
+        "Sessão inválida ou expirada.",
       );
       return;
     }
     response.setCookie("session", sessionId);
-    response.success("Sessão válida!", userOrError);
+    response.success({ user });
   } catch (error) {
     console.error("Erro ao validar sessão: ", error);
     response.internalServerError();
