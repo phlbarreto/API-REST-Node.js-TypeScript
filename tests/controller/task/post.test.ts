999bb81918d717b85ac1031bb278a08b6d1e3224
Pedro Barreto <phlbarreto90@gmail.com>
2026-04-18 18:39:57 -0300

    feat: implementa tests para novos limites na criação e alteração de tarefas

--- tests/controller/task/post.test.ts
+++ tests/controller/task/post.test.ts
@@ -11,153 +11,252 @@ beforeAll(async () => {
 });
 
 describe("POST `/tasks`", () => {
-  test("Sem usuário logado", async () => {
-    const response = await fetch(API.task, {
-      method: "POST",
-      body: JSON.stringify({
-        title: "Usuário sem sessão",
-        description: "O sistema deve bloquear esta ação!",
-      }),
-    });
+  describe("Usuário anônimo", () => {
+    test("Tentando criar tarefa", async () => {
+      const response = await fetch(API.task, {
+        method: "POST",
+        body: JSON.stringify({
+          title: "Usuário sem sessão",
+          description: "O sistema deve bloquear esta ação!",
+        }),
+      });
 
-    expect(response.ok).toBe(false);
-    expect(response.status).toBe(401);
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(401);
 
-    const responseBody = await response.json();
-    expect(responseBody).toEqual({
-      message: "Não autorizado.",
-      action: "Faça o login.",
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        message: "Não autorizado.",
+        action: "Faça o login.",
+      });
     });
   });
 
-  let defaultUser: AuthenticatedUser;
-  let defaultSession: UserSession;
-  test("Com usuário logado e todos os dados corretos", async () => {
-    const newTask = {
-      title: "Task com usuário logado",
-      description: "O sistema deve aceitar esta ação!",
-      status: "in_progress",
-    };
+  describe("Usuário padrão", () => {
+    let defaultUser: AuthenticatedUser;
+    let defaultSession: UserSession;
+    test("Todos os dados corretos", async () => {
+      const newTask = {
+        title: "Task com usuário logado",
+        description: "O sistema deve aceitar esta ação!",
+        status: "in_progress",
+      };
 
-    defaultUser = await createUser();
-    defaultSession = await createSession(defaultUser);
+      defaultUser = await createUser();
+      defaultSession = await createSession(defaultUser);
 
-    const response = await fetch(API.task, {
-      method: "POST",
-      body: JSON.stringify(newTask),
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `session=${defaultSession.id}`,
-      },
+      const response = await fetch(API.task, {
+        method: "POST",
+        body: JSON.stringify(newTask),
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+      });
+
+      expect(response.ok).toBe(true);
+      expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+      expect(responseBody).toHaveProperty("message");
+      expect(responseBody.data).toHaveProperty(
+        "description",
+        newTask.description,
+      );
+      expect(responseBody.data).toHaveProperty("title", newTask.title);
+      expect(responseBody.data).toHaveProperty("id");
+      expect(responseBody.data).toHaveProperty("created_at");
+      expect(responseBody.data).toHaveProperty("updated_at");
+      expect(responseBody.data).toHaveProperty("status");
+      expect(responseBody.data.user_id).toBe(defaultUser.id);
     });
 
-    expect(response.ok).toBe(true);
-    expect(response.status).toBe(201);
+    test("Sem a propriedade `title`", async () => {
+      const newTaskWithNoTitle = {
+        description: "O sistema deve negar esta ação!",
+      };
 
-    const responseBody = await response.json();
-    expect(responseBody).toHaveProperty("message");
-    expect(responseBody.data).toHaveProperty(
-      "description",
-      newTask.description,
-    );
-    expect(responseBody.data).toHaveProperty("title", newTask.title);
-    expect(responseBody.data).toHaveProperty("id");
-    expect(responseBody.data).toHaveProperty("created_at");
-    expect(responseBody.data).toHaveProperty("updated_at");
-    expect(responseBody.data).toHaveProperty("status");
-    expect(responseBody.data.user_id).toBe(defaultUser.id);
-  });
+      const response = await fetch(API.task, {
+        method: "POST",
+        body: JSON.stringify(newTaskWithNoTitle),
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+      });
 
-  test("Com usuário logado e sem a propriedade `title`", async () => {
-    const newTaskWithNoTitle = {
-      description: "O sistema deve negar esta ação!",
-    };
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(400);
 
-    const response = await fetch(API.task, {
-      method: "POST",
-      body: JSON.stringify(newTaskWithNoTitle),
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `session=${defaultSession.id}`,
-      },
+      const responseBody = await response.json();
+      expect(responseBody).toHaveProperty("error");
+
+      expect(responseBody.error).toBe("Coluna 'title' é obrigatório!");
     });
 
-    expect(response.ok).toBe(false);
-    expect(response.status).toBe(400);
+    test("Sem a propriedade `description`", async () => {
+      const newTaskWithNoDescription = {
+        title: "Bad request!",
+      };
 
-    const responseBody = await response.json();
-    expect(responseBody).toHaveProperty("error");
+      const response = await fetch(API.task, {
+        method: "POST",
+        body: JSON.stringify(newTaskWithNoDescription),
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+      });
 
-    expect(responseBody.error).toBe("Coluna 'title' é obrigatório!");
-  });
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(400);
 
-  test("Com usuário logado e sem a propriedade `description`", async () => {
-    const newTaskWithNoDescription = {
-      title: "Bad request!",
-    };
+      const responseBody = await response.json();
+      expect(responseBody).toHaveProperty("error");
 
-    const response = await fetch(API.task, {
-      method: "POST",
-      body: JSON.stringify(newTaskWithNoDescription),
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `session=${defaultSession.id}`,
-      },
+      expect(responseBody.error).toBe("Coluna 'description' é obrigatória!");
     });
 
-    expect(response.ok).toBe(false);
-    expect(response.status).toBe(400);
+    test("Sem as propriedades `title` e `description`", async () => {
+      const emptyObject = {};
 
-    const responseBody = await response.json();
-    expect(responseBody).toHaveProperty("error");
+      const response = await fetch(API.task, {
+        method: "POST",
+        body: JSON.stringify(emptyObject),
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+      });
 
-    expect(responseBody.error).toBe("Coluna 'description' é obrigatória!");
-  });
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(400);
 
-  test("Com usuário logado e sem as propriedades `title` e `description`", async () => {
-    const emptyObject = {};
+      const responseBody = await response.json();
+      expect(responseBody).toHaveProperty("error");
 
-    const response = await fetch(API.task, {
-      method: "POST",
-      body: JSON.stringify(emptyObject),
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `session=${defaultSession.id}`,
-      },
+      expect(responseBody.error).toBe(
+        "Coluna 'title' é obrigatório! | Coluna 'description' é obrigatória!",
+      );
     });
 
-    expect(response.ok).toBe(false);
-    expect(response.status).toBe(400);
+    test("Com `status` inválido", async () => {
+      const newTaskWithInvalidStatus = {
+        title: "Tarefa com status invalido",
+        description: "O sistema deve obrigatóriamente bloquear esta ação.",
+        status: "status_inválido",
+      };
+      const response = await fetch(API.task, {
+        method: "POST",
+        body: JSON.stringify(newTaskWithInvalidStatus),
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+      });
 
-    const responseBody = await response.json();
-    expect(responseBody).toHaveProperty("error");
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(400);
 
-    expect(responseBody.error).toBe(
-      "Coluna 'title' é obrigatório! | Coluna 'description' é obrigatória!",
-    );
-  });
-
-  test("Com usuário logado e com `status` inválido", async () => {
-    const newTaskWithInvalidStatus = {
-      title: "Tarefa com status invalido",
-      description: "O sistema deve obrigatóriamente bloquear esta ação.",
-      status: "status_inválido",
-    };
-    const response = await fetch(API.task, {
-      method: "POST",
-      body: JSON.stringify(newTaskWithInvalidStatus),
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `session=${defaultSession.id}`,
-      },
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        error:
+          'Opção inválida: esperada uma das "pending"|"in_progress"|"done"',
+      });
     });
 
-    expect(response.ok).toBe(false);
-    expect(response.status).toBe(400);
+    test("Com `title` menor que 5 caracteres", async () => {
+      const response = await fetch(API.task, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+        body: JSON.stringify({
+          title: "Pequ",
+          description: "Titulo muito pequeno, sistema deve bloquear!",
+        }),
+      });
 
-    const responseBody = await response.json();
-    expect(responseBody).toEqual({
-      error: 'Opção inválida: esperada uma das "pending"|"in_progress"|"done"',
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        error: "O título deve conter no mínimo 5 caracteres.",
+      });
+    });
+
+    test("Com `title` maior que 200 caracteres", async () => {
+      const title =
+        "Exploração Avançada de Parâmetros de Validação para Títulos e Descrições em Arquiteturas de Microserviços Modernas que Utilizam TypeScript e Node.js para Garantir a Integridade Total dos Dados na Nuvem.";
+
+      const response = await fetch(API.task, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+        body: JSON.stringify({
+          title,
+          description: "Titulo muito grande, sistema deve bloquear!",
+        }),
+      });
+
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        error: "O título deve conter no máximo 200 caracteres.",
+      });
+    });
+
+    test("Com `description` menor que 10 caracteres", async () => {
+      const response = await fetch(API.task, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+        body: JSON.stringify({
+          title: "Descrição pequena!",
+          description: "Pequena",
+        }),
+      });
+
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        error: "A descrição deve conter no mínimo 10 caracteres.",
+      });
+    });
+
+    test("Com `description` maior que 500 caracteres", async () => {
+      const description =
+        "A implementação de limites rigorosos de caracteres em campos de texto é uma prática indispensável para o desenvolvimento de APIs resilientes e escaláveis. Ao definir um teto para a descrição, o desenvolvedor previne abusos de armazenamento e garante que a transferência de dados via JSON permaneça leve, otimizando o tempo de resposta do servidor. Além disso, essa restrição facilita a padronização estética na interface do usuário, evitando que componentes de cartão ou listas sofram deformações visuais inesperadas. No contexto de SEO, manter a descrição concisa auxilia os motores de busca a indexarem corretamente o resumo do conteúdo, refletindo diretamente na relevância do projeto.";
+
+      const response = await fetch(API.task, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session=${defaultSession.id}`,
+        },
+        body: JSON.stringify({
+          title: "Descrição muito grande!",
+          description,
+        }),
+      });
+
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        error: "A descrição deve conter no máximo 500 caracteres.",
+      });
     });
   });
 });
