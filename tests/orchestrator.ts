d941406dcab57283e2ab19a715a7f47187ee64a6
Pedro Barreto <phlbarreto90@gmail.com>
2026-04-19 14:35:55 -0300

    refactor: mudanças nos models

--- tests/orchestrator.ts
+++ tests/orchestrator.ts
@@ -1,10 +1,10 @@
 import retry from "async-retry";
 import { faker } from "@faker-js/faker";
-import { registerUser } from "~/models/user.js";
+import { userModel } from "~/models/user.js";
 import { UserInput } from "~/zod/userSchema.js";
-import * as session from "~/models/session.js";
+import { AuthenticatedUser, sessionModel } from "~/models/session.js";
 import { taskModel } from "~/models/task.js";
-import { Task } from "~/zod/taskSchema.js";
+import { TaskInput } from "~/zod/taskSchema.js";
 
 const apiBaseUrl = "http://localhost:5001";
 
@@ -30,18 +30,18 @@ export async function waitForAllServices() {
 }
 
 export async function createUser(userValues?: UserInput) {
-  return await registerUser({
+  return await userModel.create({
     name: userValues?.name ?? faker.person.fullName().slice(0, 30),
     email: userValues?.email ?? faker.internet.email(),
     password: userValues?.password ?? "ValidPassword",
   });
 }
 
-export async function createSession(user: session.AuthenticatedUser) {
-  return await session.createSession(user.id);
+export async function createSession(user: AuthenticatedUser) {
+  return await sessionModel.create(user.id);
 }
 
-export async function createTask(task: Task, user: session.AuthenticatedUser) {
+export async function createTask(task: TaskInput, user: AuthenticatedUser) {
   return await taskModel.create(task, user.id);
 }
 
