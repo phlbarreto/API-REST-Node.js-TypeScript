00c1945e1e487b699d6e96cd366efcbba8659f1b
Pedro Barreto <phlbarreto90@gmail.com>
2026-04-19 14:37:44 -0300

    refactor: melhora describe

--- tests/controller/user/get.test.ts
+++ tests/controller/user/get.test.ts
@@ -20,103 +20,107 @@ describe("GET `/user`", () => {
     vi.useRealTimers();
   });
 
-  test("Sem sessão vinculada", async () => {
-    const response = await fetch(API.user);
-    expect(response.ok).toBe(false);
-    expect(response.status).toBe(401);
+  describe("Usuário anônimo", () => {
+    test("Tentando buscar os dados", async () => {
+      const response = await fetch(API.user);
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(401);
 
-    const responseBody = await response.json();
-    expect(responseBody).toEqual({
-      message: "Não autorizado.",
-      action: "Verifique se está logado e tente novamente.",
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        message: "Não autorizado.",
+        action: "Verifique se está logado e tente novamente.",
+      });
     });
   });
 
-  test("Com Sessão vinculada", async () => {
-    const newUser = await createUser();
-    const session = await createSession(newUser);
-    const response = await fetch(API.user, {
-      headers: {
-        Cookie: `session=${session.id}`,
-      },
+  describe("Usuário padrão", () => {
+    test("Com Sessão vinculada", async () => {
+      const newUser = await createUser();
+      const session = await createSession(newUser);
+      const response = await fetch(API.user, {
+        headers: {
+          Cookie: `session=${session.id}`,
+        },
+      });
+
+      expect(response.ok).toBe(true);
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        user: {
+          id: newUser.id,
+          name: newUser.name,
+          email: newUser.email,
+          created_at: newUser.created_at.toISOString(),
+          updated_at: newUser.updated_at.toISOString(),
+        },
+      });
     });
 
-    expect(response.ok).toBe(true);
-    expect(response.status).toBe(200);
+    test("Com sessão quase expirada", async () => {
+      const horaAtual = new Date();
+      const horaSessaoExpirada = new Date(
+        horaAtual.getTime() - env.INACTIVITY_TIMEOUT / 2,
+      );
+      vi.setSystemTime(horaSessaoExpirada);
 
-    const responseBody = await response.json();
-    expect(responseBody).toEqual({
-      user: {
-        id: newUser.id,
-        name: newUser.name,
-        email: newUser.email,
-        created_at: newUser.created_at.toISOString(),
-        updated_at: newUser.updated_at.toISOString(),
-      },
-    });
-  });
+      const newUser = await createUser();
+      const session = await createSession(newUser);
 
-  test("Com sessão quase expirada", async () => {
-    const horaAtual = new Date();
-    const horaSessaoExpirada = new Date(
-      horaAtual.getTime() - env.INACTIVITY_TIMEOUT / 2,
-    );
-    vi.setSystemTime(horaSessaoExpirada);
+      vi.useRealTimers();
+      const response = await fetch(API.user, {
+        headers: {
+          Cookie: `session=${session.id}`,
+        },
+      });
 
-    const newUser = await createUser();
-    const session = await createSession(newUser);
+      expect(response.ok).toBe(true);
+      expect(response.status).toBe(200);
 
-    vi.useRealTimers();
-    const response = await fetch(API.user, {
-      headers: {
-        Cookie: `session=${session.id}`,
-      },
-    });
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        user: {
+          id: newUser.id,
+          name: newUser.name,
+          email: newUser.email,
+          created_at: newUser.created_at.toISOString(),
+          updated_at: newUser.updated_at.toISOString(),
+        },
+      });
 
-    expect(response.ok).toBe(true);
-    expect(response.status).toBe(200);
-
-    const responseBody = await response.json();
-    expect(responseBody).toEqual({
-      user: {
-        id: newUser.id,
-        name: newUser.name,
-        email: newUser.email,
-        created_at: newUser.created_at.toISOString(),
-        updated_at: newUser.updated_at.toISOString(),
-      },
+      expect(new Date(session.expires_at).getTime()).greaterThan(
+        horaAtual.getTime(),
+      );
+      expect(session.created_at.getTime()).toBeLessThan(horaAtual.getTime());
     });
 
-    expect(new Date(session.expires_at).getTime()).greaterThan(
-      horaAtual.getTime(),
-    );
-    expect(session.created_at.getTime()).toBeLessThan(horaAtual.getTime());
-  });
+    test("Com sessão expirada", async () => {
+      const horaAtual = new Date();
+      const horaSessaoExpirada = new Date(
+        horaAtual.getTime() - env.INACTIVITY_TIMEOUT,
+      );
+      vi.setSystemTime(horaSessaoExpirada);
 
-  test("Com sessão expirada", async () => {
-    const horaAtual = new Date();
-    const horaSessaoExpirada = new Date(
-      horaAtual.getTime() - env.INACTIVITY_TIMEOUT,
-    );
-    vi.setSystemTime(horaSessaoExpirada);
+      const newUser = await createUser();
+      const session = await createSession(newUser);
 
-    const newUser = await createUser();
-    const session = await createSession(newUser);
+      vi.useRealTimers();
+      const response = await fetch(API.user, {
+        headers: {
+          Cookie: `session=${session.id}`,
+        },
+      });
 
-    vi.useRealTimers();
-    const response = await fetch(API.user, {
-      headers: {
-        Cookie: `session=${session.id}`,
-      },
-    });
+      expect(response.ok).toBe(false);
+      expect(response.status).toBe(401);
 
-    expect(response.ok).toBe(false);
-    expect(response.status).toBe(401);
-
-    const responseBody = await response.json();
-    expect(responseBody).toEqual({
-      message: "Sessão inválida ou expirada.",
-      action: "Por favor, realize o login novamente.",
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        message: "Sessão inválida ou expirada.",
+        action: "Por favor, realize o login novamente.",
+      });
     });
   });
 });
